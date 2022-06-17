95f3826f4f3d9b5a379d58815649a1aa06020cf0
47erbot <developer@47deg.com>
2022-06-17 07:34:26 +0200

    Update cats-core to 2.8.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val V = new {
       val bm4                     = "0.3.1"
-      val cats: String            = "2.7.0"
+      val cats: String            = "2.8.0"
       val circe: String           = "0.14.2"
       val expecty                 = "0.15.4"
       val http4s: String          = "0.23.12"
